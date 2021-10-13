99183586edd8dfe8c9e33a687933f237ce1de037
Shaun Viguerie <shaun.viguerie@athanorcapital.com>
2021-10-13 11:45:52 -0400

    add melpa back to package archives

--- lisp/sv-pkgs.el
+++ lisp/sv-pkgs.el
@@ -4,7 +4,7 @@
 ;;
 
 (require 'package)
-;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 (setq package-check-signature nil)
@@ -12,7 +12,7 @@
 
 (eval-when-compile
   (when (not (package-installed-p 'use-package))
-    ;; (package-refresh-contents)
+    (package-refresh-contents)
     (package-install 'use-package))
   (require 'use-package)
   (setq use-package-always-ensure t))
