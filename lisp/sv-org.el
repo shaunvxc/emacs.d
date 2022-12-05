f55e41943d1034635551400ac5fbc9c6de8fc2d1
sviguerie <sviguerie@veritionfund.com>
2022-12-05 11:25:35 -0500

    categorize my org keywords

--- lisp/sv-org.el
+++ lisp/sv-org.el
@@ -27,7 +27,7 @@
 (require 'org)
 
 (setq org-todo-keywords
-      '((sequence "TODO" "IN PROGRESS" "TESTING" "|" "DONE" "DEPLOYED")))
+      '((sequence "TODO" "|" "SCHEDULED" "|" "IN PROGRESS" "TESTING" "|" "DONE" "DEPLOYED")))
 
 ;; (use-package org-roam
 ;;   :ensure t
