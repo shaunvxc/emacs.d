9c7439a4ff55049441b86f8f9690607523f6d101
sviguerie <3979753+shaunvxc@users.noreply.github.com>
2023-04-21 09:45:22 -0400

    add custom.el

---
+++ custom.el
@@ -0,0 +1,38 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(frame-resize-pixelwise t)
+ '(indicate-buffer-boundaries 'left)
+ '(ring-bell-function 'ignore)
+ '(scroll-bar-mode nil)
+ '(show-paren-mode t)
+ '(use-dialog-box nil)
+ '(vc-follow-symlinks t)
+ '(visual-line-fringe-indicators '(left-curly-arrow right-curly-arrow)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(completions-common-part ((t (:foreground "#1D1F21" :background "#ffffaf"))))
+ '(cursor ((t (:background "#ffffaf"))))
+ '(diff-refine-changed ((t (:extend t))))
+ '(doom-modeline-buffer-modified ((t (:inherit (bold) :foreground "#ffffaf"))))
+ '(error ((t (:foreground "#ffffaf" :background "#000000"))))
+ '(fringe ((t (:inherit (shadow)))))
+ '(internal-border ((t (:background "#373B41"))))
+ '(isearch-fail ((t (:inherit (error)))))
+ '(minibuffer-prompt ((t (:inherit (bold) :foreground "#ffffaf"))))
+ '(org-habit-overdue-face ((t (:background "dark violet"))))
+ '(org-scheduled-previously ((t (:foreground "deep pink"))))
+ '(pulse-highlight-start-face ((t (:background "#ffffaf"))))
+ '(region ((t (:foreground "#1D1F21" :background "plum" :extend t))))
+ '(secondary-selection ((t (:foreground "#EEE2FF" :background "plum" :extend t))))
+ '(show-paren-match ((t (:inherit (bold) :foreground "#ffffaf"))))
+ '(show-paren-mismatch ((t (:inherit (error) :inverse-video t))))
+ '(warning ((t (:foreground "#ffffaf" :background "#000000"))))
+ '(window-divider ((t (:foreground "#373B41"))))
+ '(window-divider-first-pixel ((t (:foreground "#373B41"))))
+ '(window-divider-last-pixel ((t (:foreground "#373B41")))))
