672261bface144fc3f70ed52b9209979d26532d4
sviguerie <3979753+shaunvxc@users.noreply.github.com>
2023-10-24 16:46:55 -0400

    add org-capture tmplates

--- lisp/sv-org.el
+++ lisp/sv-org.el
@@ -43,6 +43,12 @@
      ))
 )
 
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/org/tasks.org" "Tasks")
+         "* TODO %?\n  %i\n  %a")
+        ("j" "Journal" entry (file+datetree "~/org/journal.org")
+         "* %?\nEntered on %U\n  %i\n  %a")))
+
 
 (setq org-startup-with-inline-images t)
 (setq org-agenda-window-setup 'current-window)
