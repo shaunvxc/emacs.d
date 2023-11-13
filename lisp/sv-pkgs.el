598f437434b055c14a1f441bcfa7b7870149ace5
sviguerie <3979753+shaunvxc@users.noreply.github.com>
2023-11-13 12:49:15 -0500

    add jcs-elpa to package repo list

--- lisp/sv-pkgs.el
+++ lisp/sv-pkgs.el
@@ -6,6 +6,7 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("jcs-elpa" . "https://jcs-emacs.github.io/jcs-elpa/packages/") t)
 
 (setq package-check-signature nil)
 (package-initialize)
